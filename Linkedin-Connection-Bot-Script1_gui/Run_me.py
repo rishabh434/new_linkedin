9ea5417df94cbc4d20425670642f0ce614f519b0
Rishabh pratap singh <156455104+rishabh434@users.noreply.github.com>
2024-06-01 10:50:28 +0530

    Add files via upload

---
+++ Linkedin-Connection-Bot-Script1_gui/Run_me.py
@@ -0,0 +1,10 @@
+#############################################
+__author__ = "Rishabh_Pratap_Singh"                  #
+__LinkedIn__ = "Rishabh_Pratap_Singh"                #
+__Github__ = "Rishabh_Pratap_Singh"                  #
+#############################################
+
+
+from Scripts import graphical_script
+if __name__ == '__main__':
+    graphical_script.guibuild()
